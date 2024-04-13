28d5d4ee25f17a11f291ede815b9711e99748a64
FAGunin <fagunin@mai.education>
2024-04-13 23:42:32 +0300

    upd main
    added:
    - face tracking
    - more keys to start/stop tracker wile program is running
    - labels in frame for active trackers

--- main.py
+++ main.py
@@ -15,46 +15,75 @@ def get_frame(cap):
 
 
 if __name__ == "__main__":
-    # Make a tracker and turn on the camera
-    tracker = cv2.TrackerCSRT_create()
     # NOTE: Select number of your camera 0 or 1 or 2 …
     cap = cv2.VideoCapture(2)
     time.sleep(1)
 
-    # Get the first frame and select the object
-    # and initialize the tracker
-    frame, gray = get_frame(cap)
-    object = cv2.selectROI(gray)
-    tracker.init(gray, object)
+    # Tracker
+    tracker = cv2.TrackerCSRT_create()
+    TRACK = False
 
-    print("Start tracking!")
-    print("Press Esc to exit\n")
+    # Auto trackers
+    # Face
+    faces = cv2.CascadeClassifier(
+        "training/haarcascade_frontalface_default.xml")
+    FACE = False
 
-    # Variables for backuping
     bf_cout = 0
 
     # Main loop
     while True:
-        # Get the frame and update the tracker
         frame, gray = get_frame(cap)
-        success, box = tracker.update(gray)
 
-        # print("Frame: ", bf_cout)
+        tracker_number = 1
+        if TRACK:
+            # Update the tracker
+            success, box = tracker.update(gray)
+            m.draw_tracker_name(frame, "CSRT tracker")
 
-        # If the object is tracked on the frame, draw a rectangle
-        if success:
-            m.draw_rect(frame, box=box)
+            # If the object is tracked on the frame, draw a rectangle
+            if success:
+                m.draw_rect(frame, box=box)
+
+        if FACE:
+            if TRACK:
+                tracker_number = 2
+            m.draw_tracker_name(frame, "FACE tracker", tracker_number)
+            m.draw_rect(frame, rect=m.detect_face(gray, faces))
 
         # Count the frames
         bf_cout += 1
+        # print("Frame: ", bf_cout)
 
         # Show the frame
         cv2.imshow("FRAME", frame)
 
-        # Wait for s key to reselect the object
+        # Wait for f key to start face tracking
+        if cv2.waitKey(1) & 0xFF == ord("f"):
+            FACE = True
+            continue
+        # Wait for F key to stop face tracking
+        if cv2.waitKey(1) & 0xFF == ord("F"):
+            FACE = False
+            continue
+
+        # Wait for s key to reselect the object for Tracker
         if cv2.waitKey(1) & 0xFF == ord("s"):
             object = cv2.selectROI(gray)
             tracker.init(gray, object)
+            TRACK = True
+            continue
+        # Wait for S key to stop CSRT Tracker
+        if cv2.waitKey(1) & 0xFF == ord("S"):
+            TRACK = False
+            continue
+
+        # Wait for Q key to stop all trackers
+        if cv2.waitKey(1) & 0xFF == ord("Q"):
+            if TRACK:
+                TRACK = False
+            if FACE:
+                FACE = False
             continue
 
         # Wait for Esc key to stop
